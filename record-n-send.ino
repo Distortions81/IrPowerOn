80a3da350ba0f8967a11404820480b2d158b4390
Distortions81 <carlotto81@gmail.com>
2021-03-17 22:11:01 -0600

    init

---
+++ record-n-send.ino
@@ -0,0 +1,119 @@
+/*
+   SimpleReceiver.cpp
+
+   Demonstrates receiving NEC IR codes with IRrecv
+
+    Copyright (C) 2020-2021  Armin Joachimsmeyer
+    armin.joachimsmeyer@gmail.com
+
+    This file is part of Arduino-IRremote https://github.com/z3t0/Arduino-IRremote.
+
+    MIT License
+*/
+
+/*
+   Specify which protocol(s) should be used for decoding.
+   If no protocol is defined, all protocols are active.
+*/
+#define DECODE_NEC 1
+
+#include <IRremote.h>
+#include <EEPROM.h>
+
+/*
+   Set sensible receive pin for different CPU's
+*/
+#if defined(__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__) || defined(__AVR_ATtiny87__) || defined(__AVR_ATtiny167__)
+// Serial output for Digispark boards is at pin 2
+#  if defined(ARDUINO_AVR_DIGISPARKPRO)
+#define IR_RECEIVE_PIN    9 // PA3 - on Digispark board labeled as pin 9
+#  else
+#define IR_RECEIVE_PIN    0
+#  endif
+#  if defined(ARDUINO_AVR_DIGISPARK)
+#define LED_BUILTIN PB1
+#  endif
+#elif defined(ESP32)
+int IR_RECEIVE_PIN = 15;
+#elif defined(ARDUINO_AVR_PROMICRO)
+int IR_RECEIVE_PIN = 10;
+#else
+int IR_RECEIVE_PIN = 11;
+#endif
+
+uint32_t saveAddr = 0;
+uint32_t saveCmd = 0;
+uint32_t saveProto = 0;
+
+void setup() {
+
+  Serial.begin(115200);
+  // Just to know which program is running on my Arduino
+  Serial.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing library version " VERSION_IRREMOTE));
+
+  /*
+     Start the receiver, enable feedback LED and take LED feedback pin from the internal boards definition
+  */
+  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK, USE_DEFAULT_FEEDBACK_LED_PIN);
+
+
+  Serial.print("Addr ");
+  Serial.println(EEPROM.read(0));
+  Serial.print("Cmd ");
+  Serial.println(EEPROM.read(1));
+  Serial.print("Proto ");
+  Serial.println(EEPROM.read(2));
+
+  Serial.print(F("Ready to receive IR signals at pin "));
+  Serial.println(IR_RECEIVE_PIN);
+}
+
+void loop() {
+  /*
+     Check if received data is available and if yes, try to decode it.
+     Decoded result is in the IrReceiver.decodedIRData structure.
+
+     E.g. command is in IrReceiver.decodedIRData.command
+     address is in command is in IrReceiver.decodedIRData.address
+     and up to 32 bit raw data in IrReceiver.decodedIRData.decodedRawData
+  */
+  if (IrReceiver.decode()) {
+
+    // Print a short summary of received data
+    IrReceiver.printIRResultShort(&Serial);
+    if (IrReceiver.decodedIRData.protocol == UNKNOWN) {
+      // We have an unknown protocol here, print more info
+      IrReceiver.printIRResultRawFormatted(&Serial, true);
+    }
+    Serial.println();
+
+    if ( IrReceiver.decodedIRData.flags & IRDATA_FLAGS_IS_AUTO_REPEAT || IrReceiver.decodedIRData.flags & IRDATA_FLAGS_IS_REPEAT ) {
+      Serial.println("repeat, not saving to EEPROM.");
+    } else {
+      saveAddr = IrReceiver.decodedIRData.address;
+      EEPROM.write(0, saveAddr);
+      saveCmd = IrReceiver.decodedIRData.command;
+      EEPROM.write(1, saveCmd);
+      saveProto = IrReceiver.decodedIRData.protocol;
+      EEPROM.write(2, saveProto);
+
+      Serial.println("Saved to EEPROM.");
+    }
+
+
+    /*
+       !!!Important!!! Enable receiving of the next value,
+       since receiving has stopped after the end of the current received data packet.
+    */
+    IrReceiver.resume(); // Enable receiving of the next value
+
+    /*
+       Finally, check the received data and perform actions according to the received command
+    */
+    if (IrReceiver.decodedIRData.command == 0x10) {
+      // do something
+    } else if (IrReceiver.decodedIRData.command == 0x11) {
+      // do something else
+    }
+  }
+}
