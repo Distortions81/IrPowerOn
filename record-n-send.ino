1d60a4fba441bf2e811ca699db3eb5b660dc88fa
Distortions81 <carlotto81@gmail.com>
2021-03-18 15:03:58 -0600

    normal/program mode, led blink, util functions

--- record-n-send.ino
+++ record-n-send.ino
@@ -3,73 +3,139 @@
 #include <IRremote.h>
 #include <EEPROM.h>
 
-int IR_RECEIVE_PIN = 11;
+//GPIO
+const int IR_RECEIVE_PIN = 11;
+const int ledPin =  LED_BUILTIN;
+const int programButtonPin = 2;
 
+//EEPROM vars
 uint32_t saveAddr = 0;
 uint32_t saveCmd = 0;
 
-bool programMode = true;
+uint32_t lastAddr = 0;
+uint32_t lastCmd = 0;
+
+//Mode, same code rev count
+bool programMode = false;
 int sameCodeCount = 0;
-unsigned long prevBlinkMillis = 0;
+
+//LED Blink
+unsigned long previousMillis = 0;
 bool ledOn = false;
+const long intervalProgram = 250;
 
-void setup()
-{
+//Normal mode: Code send delay, repeats, repeat delays
+const int sendDelay = 30;
+const int sendRepeats = 5;
+const int sendRepeatDelay = 1;
 
+//Poll interval, 1/10th of a second
+const int pollInterval = 100;
+
+void setup() {
+
+  //Setup serial, IR recv, and status LED.
   Serial.begin(115200);
-  IrReceiver.begin(IR_RECEIVE_PIN, DISABLE_LED_FEEDBACK);
+  Serial.println("");
+  Serial.println("Starting...");
 
+  pinMode(ledPin, OUTPUT);
+  pinMode(programButtonPin, INPUT_PULLUP);
+
+
+  saveAddr = EEPROM.read(0);
+  saveCmd = EEPROM.read(1);
+
+  if (saveCmd > 0) {
+    goNormalMode();
+
+  } else {
+    goProgramMode();
+
+  }
+}
+
+void clearTemp() {
+  saveCmd = 0;
+  saveAddr = 0;
+  lastCmd = 0;
+  lastAddr = 0;
+  sameCodeCount = 0;
+
+  previousMillis = 0;
+}
+
+void goProgramMode() {
+  programMode = true;
+  clearTemp();
+  Serial.println("Entering program mode.");
+
+  IrReceiver.begin(IR_RECEIVE_PIN, DISABLE_LED_FEEDBACK);
+  Serial.print(F("Enabling IR receiver on pin "));
+  Serial.println(IR_RECEIVE_PIN);
+}
+
+void goNormalMode() {
+  programMode = false;
+  clearTemp();
+
+  //Clear status LED.
+  digitalWrite(ledPin, LOW );
+  ledOn = false;
+
+  Serial.println("Entering normal mode.");
   Serial.println("Reading values from EEPROM.");
   saveAddr = EEPROM.read(0);
   saveCmd = EEPROM.read(1);
 
   Serial.print("Addr ");
   Serial.println(saveAddr);
-
   Serial.print("Cmd ");
-  Serial.println(saveAddr);
+  Serial.println(saveCmd);
 
-  if (saveCmd > 0)
-  {
-    programMode = false;
-    Serial.println("Mode: Normal/Send code");
-  }
-  else
-  {
-    programMode = true;
-    Serial.println("Mode: Program/Record");
-  }
-
-  Serial.print(F("Ready to receive IR signals at pin "));
-  Serial.println(IR_RECEIVE_PIN);
+  IrReceiver.stop();
+  Serial.println("Disabling IR receiver.");
 }
 
-void loop()
-{
-  if (programMode)
-  {
-    if (IrReceiver.decode())
-    {
+void loop() {
+  //Sleep for a bit
+  delay(pollInterval);
+  
+  if (programMode) {
 
-      // Print a short summary of received data
-      IrReceiver.printIRResultShort(&Serial);
-      if (IrReceiver.decodedIRData.protocol == UNKNOWN)
-      {
-        // We have an unknown protocol here, print more info
-        IrReceiver.printIRResultRawFormatted(&Serial, true);
-      }
-      Serial.println();
+    //Check if it is time to blink status LED, to indicate program mode.
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= intervalProgram) {
+      previousMillis = currentMillis;
 
-      if (IrReceiver.decodedIRData.flags & IRDATA_FLAGS_IS_AUTO_REPEAT || IrReceiver.decodedIRData.flags & IRDATA_FLAGS_IS_REPEAT)
-      {
-        Serial.println("repeat, not saving to EEPROM.");
+      //Blink LED
+      if (ledOn ) {
+        ledOn = false;
+        digitalWrite(ledPin, LOW );
+      } else {
+        ledOn = true;
+        digitalWrite(ledPin, HIGH );
       }
-      else
-      {
+    }
+
+    if (IrReceiver.decode()) {
+
+      Serial.println("");
+      if (IrReceiver.decodedIRData.protocol == UNKNOWN) {
+        Serial.println("Unknown protocol, ignored.");
+      } else {
+        Serial.print("Addr ");
+        Serial.println(IrReceiver.decodedIRData.address);
+
+        Serial.print("Cmd ");
+        Serial.println(IrReceiver.decodedIRData.command);
+      }
+
+      if ( IrReceiver.decodedIRData.flags & IRDATA_FLAGS_IS_AUTO_REPEAT || IrReceiver.decodedIRData.flags & IRDATA_FLAGS_IS_REPEAT ) {
+        Serial.println("Repeat, not saving to EEPROM.");
+      } else {
         saveAddr = IrReceiver.decodedIRData.address;
-        EEPROM.write(0, saveAddr);
         saveCmd = IrReceiver.decodedIRData.command;
-        EEPROM.write(1, saveCmd);
 
         Serial.println("Saved to EEPROM.");
       }
@@ -79,14 +145,17 @@ void loop()
       /*
          Finally, check the received data and perform actions according to the received command
       */
-      if (IrReceiver.decodedIRData.command == 0x10)
-      {
+      if (IrReceiver.decodedIRData.command == 0x10) {
         // do something
-      }
-      else if (IrReceiver.decodedIRData.command == 0x11)
-      {
+      } else if (IrReceiver.decodedIRData.command == 0x11) {
         // do something else
       }
+    }
+  } else {
+
+    //Check for program button pressed
+    if (digitalRead(programButtonPin) == LOW) {
+      goProgramMode();
     }
   }
 }
